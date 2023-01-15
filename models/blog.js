dfab930265b8374745fdf1f1d3497f4b42c758b2
Kemal muhammad <kemalhiero@gmail.com>
2023-01-15 23:32:33 +0700

    upload gambar

--- models/blog.js
+++ models/blog.js
@@ -37,7 +37,7 @@ const blog = sequelize.define('blog', {
         allowNull : false
     }, 
     gambar : {
-        type : DataTypes.BLOB,
+        type : DataTypes.STRING,
         allowNull : true
     },
     summary: {
--- routes/blog.js
+++ routes/blog.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const controller = require(`../controllers/indexcontroller`);
 const multer = require('multer')
+const path = require('path')
 const router = express();
 router.set("view engine", "ejs");
 router.use(express.static("public"));
 
 const fileStorage = multer.diskStorage({
-    destination: (req, res, cb) => {
-      cb(null, './assets/')
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname,'../storage/'));
     },
     filename: (req, file, cb) => {
-      cb(null,file.originalname)
+      console.log(file);
+      cb(null,  Date.now().toString() +  file.originalname)
     }
   })
   
@@ -24,12 +26,20 @@ const fileStorage = multer.diskStorage({
     }
   
   }
+
+  const upload = multer({
+    storage: fileStorage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  })
   
 
 router.get("/", controller.blog.getAllBlog);
 router.get("/detail-:id", controller.blog.getBlogById);
 router.get("/addBlog",controller.blog.getBlog);
-router.post("/addBlog", controller.blog.addBlog);
+router.post("/addBlog", upload.single("gambar_blog") ,controller.blog.addBlog);
 router.put("/updateBlog/:id", controller.blog.updateBlog);
 router.delete("/deleteBlog/:id", controller.blog.deleteBlog);
 router.get("/comment/all", controller.blog.getAllComment);
